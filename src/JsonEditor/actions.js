b99f606eb0e55a8f7926278a2c913c3927fefbcc
lucasMontenegro <40044087+lucasMontenegro@users.noreply.github.com>
2019-04-10 20:23:30 -0300

    JSON Editor: new reducer for editing object keys

--- src/JsonEditor/actions.js
+++ src/JsonEditor/actions.js
@@ -1,13 +1,31 @@
-export const UPDATE_NUMBER = 'UPDATE_NUMBER';
+export const UPDATE_NUMBER = 'jsonEditor/UPDATE_NUMBER';
 export const updateNumber = (id, value) => ({
   type: UPDATE_NUMBER,
   id,
   value
 });
 
-export const UPDATE_STRING = 'UPDATE_STRING';
+export const UPDATE_STRING = 'jsonEditor/UPDATE_STRING';
 export const updateString = (id, value) => ({
   type: UPDATE_STRING,
   id,
   value
+});
+
+export const EDIT_OBJECT_KEY = 'jsonEditor/EDIT_OBJECT_KEY';
+export const editObjectKey = (id, key) => ({
+  type: EDIT_OBJECT_KEY,
+  id,
+  key
+});
+
+export const UPDATE_KEY = 'jsonEditor/UPDATE_KEY';
+export const updateKey = key => ({
+  type: UPDATE_KEY,
+  key
+});
+
+export const SAVE_KEY = 'jsonEditor/SAVE_KEY';
+export const saveKey = () => ({
+  type: SAVE_KEY
 });
--- src/JsonEditor/reducer.js
+++ src/JsonEditor/reducer.js
@@ -1,40 +1,99 @@
+import { clone } from 'lodash';
 import {
   UPDATE_NUMBER,
-  UPDATE_STRING
+  UPDATE_STRING,
+  EDIT_OBJECT_KEY,
+  UPDATE_KEY,
+  SAVE_KEY
 } from './actions';
 import { normalize } from './normalize-json';
 import data from './normalize-json/data.json';
 
-function jsonEditorRdr (state, action) {
-  state || (state = initState());
-  return {
-    ...state,
-    byID: elementsByID(state.byID, action)
-  };
+const initState = () => {
+  const state = normalize(data);
+  state.keyEditor = {};
+  return state;
 }
 
-const initState = () => normalize(data);
-
-function elementsByID (state, action) {
+function jsonEditorRdr (state, action) {
+  state || (state = initState());
   switch (action.type) {
-
     case UPDATE_NUMBER: {
       const { id, value } = action;
-      const elem = state[id];
+      const { byID } = state;
+      const elem = byID[id];
       return {
         ...state,
-        [id]: { ...elem, value: value.replace(/[^0-9]/gm, '') }
+        byID: {
+          ...byID,
+          [id]: {
+            ...elem,
+            value: value.replace(/[^0-9]/gm, '')
+          }
+        }
       };
     }
 
     case UPDATE_STRING: {
       const { id, value } = action;
-      const elem = state[id];
+      const { byID } = state;
+      const elem = byID[id];
       return {
         ...state,
-        [id]: {
-          ...elem,
-          value
+        byID: {
+          ...byID,
+          [id]: {
+            ...elem,
+            value
+          }
+        }
+      };
+    }
+
+    case EDIT_OBJECT_KEY: {
+      const { id, key } = action;
+      return {
+        ...state,
+        keyEditor: { id, key, oldKey: key }
+      };
+    }
+
+    case UPDATE_KEY: {
+      const { byID, keyEditor } = state;
+      const { id, oldKey } = keyEditor;
+      if (!id) return state;
+
+      const elem = byID[id];
+      if (!elem || elem.type !== 'object') return { ...state, keyEditor: {} };
+
+      const { key } = action;
+      if (key !== oldKey && elem.kids[key]) return state;
+
+      return { ...state, keyEditor: { ...keyEditor, key } };
+    }
+
+    case SAVE_KEY: {
+      const { byID, keyEditor } = state;
+      const { id, key, oldKey } = keyEditor;
+      if (!id) return state;
+
+      const elem = byID[id];
+      const kidID = elem.kids[key];
+      if (!elem || kidID) return state;
+
+      const kids = clone(elem.kids);
+      kids[key] = kids[oldKey];
+      delete kids[oldKey];
+
+      return {
+        ...state,
+        keyEditor: {},
+        byID: {
+          ...byID,
+          [id]: {
+            ...elem,
+            kids
+          }
         }
       };
     }

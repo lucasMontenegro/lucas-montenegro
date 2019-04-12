6f43f237813d82b24d63f6af3d586445442f0f80
lucasMontenegro <40044087+lucasMontenegro@users.noreply.github.com>
2019-04-12 16:59:08 -0300

    Some styling

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import {
-  BrowserRouter,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import styled from 'styled-components';
 
 import NavBar from './NavBar';
 import HomePage from './HomePage';
 import TodoList from '../TodoList';
 import JsonEditor from '../JsonEditor';
 
-export const PureRouter = () => (
+
+const SizingDiv = styled.div`
+  max-width: 100ch;
+  min-height: 100vh;
+  margin: 0px auto;
+  padding: 1ch;
+`;
+
+const ContentDiv = styled.div`
+  border: 0.1ch solid #03ce0b;
+  border-radius: 2px;
+  padding: 0.9ch;
+  min-height: 35em;
+`;
+
+export const PureRouter = ({ children }) => (
+  <SizingDiv>
+    <NavBar />
+    <ContentDiv>
+      {children}
+    </ContentDiv>
+  </SizingDiv>
+);
+
+
+const Router = () => (
   <BrowserRouter>
-    <div>
-      <NavBar />
+    <PureRouter>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/todo" component={TodoList} />
         <Route exact path="/json-editor" component={JsonEditor} />
       </Switch>
-    </div>
+    </PureRouter>
   </BrowserRouter>
 );
 
-export default PureRouter;
+export default Router;

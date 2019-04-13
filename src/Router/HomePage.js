62ef73fede5c9babb3f0f9a8a32c83c2b13572d9
lucasMontenegro <40044087+lucasMontenegro@users.noreply.github.com>
2019-04-12 21:31:50 -0300

    More descriptive HomePage + Spanish translation

--- src/Router/HomePage.js
+++ src/Router/HomePage.js
@@ -1,6 +1,6 @@
 import React, { Suspense } from 'react';
 import { Link } from "react-router-dom";
-import { useTranslation } from 'react-i18next';
+import { useTranslation, Trans } from 'react-i18next';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import Spinner from './Spinner';
@@ -8,20 +8,20 @@ import Spinner from './Spinner';
 export const PureHomePage = ({ t }) => (
   <main>
     <section>
-      <header><h3>{t('welcome')}</h3></header>
+      <header><h3>{t('home.welcome.header')}</h3></header>
       <p>
-        Hi! My name is Lucas Montenegro and I am a self-taught Junior Web
-        Developer from Argentina. I started learning programming in 2018 and
-        I'm currently looking to get my first software development job.
+        {t('home.welcome.hi')}
       </p>
       <p>
-        This is my personal website where I host the learning projects I'm
-        working on. Take a look at them in the page called
-        "<Link to="my-projects">My Projects</Link>".
+        <Trans i18nKey="home.welcome.thisWebsite">
+          start
+          <Link to="/my-projects">link</Link>
+          end
+        </Trans>
       </p>
     </section>
     <section>
-      <header><h3>My Skills</h3></header>
+      <header><h3>{t('home.skills.title')}</h3></header>
       <h4>Front-end</h4>
       <ul>
         <li>HTML5</li>
@@ -42,7 +42,7 @@ export const PureHomePage = ({ t }) => (
         <li>Mocha + Chai</li>
         <li>Jest</li>
       </ul>
-      <h4>Tools</h4>
+      <h4>{t('home.skills.tools.title')}</h4>
       <ul>
         <li>Git</li>
         <li>Vagrant</li>
@@ -51,7 +51,7 @@ export const PureHomePage = ({ t }) => (
       </ul>
     </section>
     <section>
-      <header><h3>Contact</h3></header>
+      <header><h3>{t('home.contact.title')}</h3></header>
       <ul>
         <li>
           <a href="https://www.linkedin.com/in/lucas-montenegro-1b191915a/">

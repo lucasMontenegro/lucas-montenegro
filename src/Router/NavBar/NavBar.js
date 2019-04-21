f801d1644e28bf012edbb1b4bb765fee58f11816
lucasMontenegro <40044087+lucasMontenegro@users.noreply.github.com>
2019-04-21 18:08:43 -0300

    Cleaning up

--- src/Router/NavBar/NavBar.js
+++ src/Router/NavBar/NavBar.js
@@ -2,30 +2,14 @@ import React, { Suspense } from 'react';
 import { withTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 
-// components
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import Badge from '@material-ui/core/Badge';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-
-// styling tools
-import { fade } from '@material-ui/core/styles/colorManipulator';
 import { withStyles } from '@material-ui/core/styles';
-
-// icons
 import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import MoreIcon from '@material-ui/icons/MoreVert';
 
 import Title from './Title';
-
 import { LanguageButton, LanguageMenu } from './language-selection';
 
 const styles = theme => ({
@@ -39,63 +23,6 @@ const styles = theme => ({
     marginLeft: -12,
     marginRight: 20,
   },
-  title: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(3),
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    width: theme.spacing(9),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-    width: '100%',
-  },
-  inputInput: {
-    paddingTop: theme.spacing(1),
-    paddingRight: theme.spacing(1),
-    paddingBottom: theme.spacing(1),
-    paddingLeft: theme.spacing(10),
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: 200,
-    },
-  },
-  sectionDesktop: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
-    },
-  },
-  sectionMobile: {
-    display: 'flex',
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
-    },
-  },
 });
 
 const PureNavBar = props => {
@@ -103,60 +30,10 @@ const PureNavBar = props => {
     t,
     i18n,
     classes,
-    languageEl,
-    anchorEl,
-    mobileMoreAnchorEl,
+    languageAnchorEl
   } = props;
 
-  const isLanguageMenuOpen = Boolean(languageEl);
-  const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMenuOpen}
-      onClose={props.handleMenuClose}
-    >
-      <MenuItem onClick={props.handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={props.handleMenuClose}>My account</MenuItem>
-    </Menu>
-  );
-
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMobileMenuOpen}
-      onClose={props.handleMenuClose}
-    >
-      <MenuItem onClick={props.handleMobileMenuClose}>
-        <IconButton color="inherit">
-          <Badge badgeContent={4} color="secondary">
-            <MailIcon />
-          </Badge>
-        </IconButton>
-        <p>Messages</p>
-      </MenuItem>
-      <MenuItem onClick={props.handleMobileMenuClose}>
-        <IconButton color="inherit">
-          <Badge badgeContent={11} color="secondary">
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-        <p>Notifications</p>
-      </MenuItem>
-      <MenuItem onClick={props.handleProfileMenuOpen}>
-        <IconButton color="inherit">
-          <AccountCircle />
-        </IconButton>
-        <p>Profile</p>
-      </MenuItem>
-    </Menu>
-  );
+  const isLanguageMenuOpen = Boolean(languageAnchorEl);
 
   return (
     <div className={classes.root}>
@@ -170,66 +47,22 @@ const PureNavBar = props => {
             <MenuIcon />
           </IconButton>
           <Typography
-            className={classes.title}
             variant="h6"
             color="inherit"
             noWrap
           >
             <Title t={t} />
           </Typography>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-            />
-          </div>
           <div className={classes.grow} />
           <LanguageButton
             open={isLanguageMenuOpen}
             onClick={props.handleLanguageMenuOpen}
           />
-          <div className={classes.sectionDesktop}>
-            <IconButton color="inherit">
-              <Badge badgeContent={4} color="secondary">
-                <MailIcon />
-              </Badge>
-            </IconButton>
-            <IconButton color="inherit">
-              <Badge badgeContent={17} color="secondary">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
-              aria-owns={isMenuOpen ? 'material-appbar' : undefined}
-              aria-haspopup="true"
-              onClick={props.handleProfileMenuOpen}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-          </div>
-          <div className={classes.sectionMobile}>
-            <IconButton
-              aria-haspopup="true"
-              onClick={props.handleMobileMenuOpen}
-              color="inherit"
-            >
-              <MoreIcon />
-            </IconButton>
-          </div>
         </Toolbar>
       </AppBar>
-      {renderMenu}
-      {renderMobileMenu}
       <LanguageMenu
         i18n={i18n}
-        anchorEl={languageEl}
+        anchorEl={languageAnchorEl}
         open={isLanguageMenuOpen}
         onClose={props.handleLanguageMenuClose}
       />
@@ -241,66 +74,35 @@ const LazyNavBar = withTranslation()(PureNavBar);
 
 class NavBar extends React.Component {
   state = {
-    languageEl: null,
-    anchorEl: null,
-    mobileMoreAnchorEl: null,
+    languageAnchorEl: null
   };
 
   handleLanguageMenuOpen = event => {
-    this.setState({ languageEl: event.currentTarget });
+    this.setState({ languageAnchorEl: event.currentTarget });
   };
 
   handleLanguageMenuClose = () => {
-    this.setState({ languageEl: null });
-  };
-
-  handleProfileMenuOpen = event => {
-    this.setState({ anchorEl: event.currentTarget });
-  };
-
-  handleMenuClose = () => {
-    this.setState({ anchorEl: null });
-    this.handleMobileMenuClose();
-  };
-
-  handleMobileMenuOpen = event => {
-    this.setState({ mobileMoreAnchorEl: event.currentTarget });
-  };
-
-  handleMobileMenuClose = () => {
-    this.setState({ mobileMoreAnchorEl: null });
+    this.setState({ languageAnchorEl: null });
   };
 
   render() {
     const {
       handleLanguageMenuOpen,
-      handleLanguageMenuClose,
-      handleProfileMenuOpen,
-      handleMenuClose,
-      handleMobileMenuOpen,
-      handleMobileMenuClose
+      handleLanguageMenuClose
     } = this;
 
     const {
-      languageEl,
-      anchorEl,
-      mobileMoreAnchorEl
+      languageAnchorEl
     } = this.state;
 
-    const { classes } = this.props;
+    const {
+      classes
+    } = this.props;
 
     const props = {
       handleLanguageMenuOpen,
       handleLanguageMenuClose,
-      handleProfileMenuOpen,
-      handleMenuClose,
-      handleMobileMenuOpen,
-      handleMobileMenuClose,
-
-      languageEl,
-      anchorEl,
-      mobileMoreAnchorEl,
-
+      languageAnchorEl,
       classes
     };
 

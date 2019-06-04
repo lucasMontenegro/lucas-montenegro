3cc9d10b2d8661c4c9912a2ebc168f14d1e4cdd2
lucasMontenegro <40044087+lucasMontenegro@users.noreply.github.com>
2019-06-04 11:36:57 -0300

    Small fix in HTTP server and build script

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const path = require("path")
 const cluster = require("cluster")
 const numCPUs = require("os").cpus().length
-const buildDirname = path.resolve(__dirname, `build`)
+const buildDirname = path.resolve(__dirname, `../build`)
 const isDev = process.env.NODE_ENV !== `production`
 const PORT = process.env.PORT || 5000
 // Multi-process to utilize all CPU cores.

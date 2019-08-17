3bce36e2182426625efb2c9fc8313a3a4c60dffa
lucasMontenegro <40044087+lucasMontenegro@users.noreply.github.com>
2019-08-17 20:34:24 -0300

    Fixed theme bug

--- cra-ui/src/localApp/index.js
+++ cra-ui/src/localApp/index.js
@@ -6,17 +6,22 @@ import store from "./store"
 import "typeface-roboto"
 import "./fontAwesome"
 import CssBaseline from "@material-ui/core/CssBaseline"
+import { ThemeProvider } from "@material-ui/styles"
+import { createMuiTheme } from "@material-ui/core/styles"
 import Examples from "./Examples"
 import App from "./App"
+const theme = createMuiTheme()
 const mainRoute = <Route component={App} />
 const app = (
   <Provider store={store}>
     <CssBaseline />
     <BrowserRouter>
-      {
-        process.env.NODE_ENV === `production`? {mainRoute} : 
-        <Switch><Route path="/examples" component={Examples} />{mainRoute}</Switch>
-      }
+      <ThemeProvider theme={theme}>
+        {
+          process.env.NODE_ENV === `production`? {mainRoute} :
+          <Switch><Route path="/examples" component={Examples} />{mainRoute}</Switch>
+        }
+      </ThemeProvider>
     </BrowserRouter>
   </Provider>
 )

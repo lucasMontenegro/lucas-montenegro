1913d9e1275753b28d60ca057a2a36261cdd002d
lucasMontenegro <40044087+lucasMontenegro@users.noreply.github.com>
2019-10-17 18:50:26 -0300

    paperbase: makeLanguageDialogState

---
+++ src/new/local/paperbase/propTypes/linkTranslationsPropType.js
@@ -0,0 +1,3 @@
+import { makeTranslationsPropType } from "new/local/utils/makeTranslations"
+import makeLocationPropType from "new/local/paperbase/propTypes/makeLocationPropType"
+export default makeTranslationsPropType(makeLocationPropType())

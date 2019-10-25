e41f8b7c040998ba0b02eed3a6ff8cbae5273931
lucasMontenegro <40044087+lucasMontenegro@users.noreply.github.com>
2019-10-25 08:13:40 -0300

    Getting rid of portals altogether

--- src/lib/examples.js
+++ src/lib/examples.js
@@ -2,12 +2,10 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter, Switch } from "react-router-dom"
 import throwPropTypeErrors from "lib/throwPropTypeErrors/example"
-import useUniqueInstance from "lib/useUniqueInstance/example"
 ReactDOM.render((
   <BrowserRouter>
     <Switch>
       {throwPropTypeErrors}
-      {useUniqueInstance}
     </Switch>
   </BrowserRouter>
 ), document.getElementById(`root`))

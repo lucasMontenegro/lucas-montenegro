9ec80986aa9b8320230710edb8dd5bc7ac9fe8b5
lucasMontenegro <40044087+lucasMontenegro@users.noreply.github.com>
2019-12-17 17:25:45 -0300

    lib/Translation: different way of checking missing translation

--- src/lib/Translation/get.js
+++ src/lib/Translation/get.js
@@ -3,15 +3,14 @@ import isProduction from "lib/utils/isProduction"
 import globals from "lib/utils/globals"
 export default function get () {
   const languageCode = languageDetector.get()
-  const value = this.source[languageCode]
-  if (value === undefined) {
-    const msg = `Language code not supported in translation: ${languageCode}`
-    if (isProduction()) {
-      globals.console.error(msg)
-    } else {
-      throw Error(msg)
-    }
-    return this.fallbackValue
+  if (languageCode in this.source) {
+    return this.source[languageCode]
   }
-  return value
+  const msg = `Language code not supported in translation: ${languageCode}`
+  if (isProduction()) {
+    globals.console.error(msg)
+  } else {
+    throw Error(msg)
+  }
+  return this.fallbackValue
 }

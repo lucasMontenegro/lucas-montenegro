6176a9e9792dfbd597eff530ac6a070ab81e2dbc
lucasMontenegro <40044087+lucasMontenegro@users.noreply.github.com>
2019-12-18 18:09:03 -0300

    lib/react/routing/context

---
+++ src/lib/react/routing/__tests__/context.js
@@ -0,0 +1,39 @@
+import React, { createContext, useMemo, useContext } from "react"
+import Router from "../Router"
+import renderer from "react-test-renderer"
+import { RoutingProvider, useRoute, useRoutingContext } from "../context"
+jest.mock(`react`, () => {
+  const actual = jest.requireActual(`react`)
+  return {
+    ...actual,
+    __esModule: true,
+    default: actual,
+    createContext: () => ({ Provider: () => null }),
+    useMemo: fn => fn(),
+    useContext: () => ({}),
+  }
+})
+jest.mock(`../Router`, () => ({
+  __esModule: true,
+  default: class { findRoute () {} },
+}))
+describe(`../context`, () => {
+  it(`should use the right versions of its dependencies`, () => {
+    expect(jestUtils.getDependencies([`react`])).toMatchSnapshot()
+  })
+  describe(`RoutingProvider`, () => {
+    it(`should run`, () => {
+      renderer.create(<RoutingProvider />)
+    })
+  })
+  describe(`useRoute`, () => {
+    it(`should run`, () => {
+      useRoute()
+    })
+  })
+  describe(`useRoutingContext`, () => {
+    it(`should run`, () => {
+      useRoutingContext()
+    })
+  })
+})
---
+++ src/lib/react/routing/context.js
@@ -0,0 +1,24 @@
+import React, { createContext, useMemo, useContext } from "react"
+import Router from "./Router"
+const RoutingContext = createContext({
+  routing: null,
+  route: {
+    render: {},
+    redirect: null,
+  },
+})
+RoutingContext.displayName = `RoutingContext`
+export function RoutingProvider ({ routing, location, children }) {
+  const router = useMemo(() => new Router(routing), [routing])
+  const value = useMemo(() => ({
+    routing,
+    route: router.findRoute(location),
+  }), [routing, router, location])
+  return <RoutingContext.Provider value={value}>{children}</RoutingContext.Provider>
+}
+export function useRoute () {
+  return useContext(RoutingContext).route
+}
+export function useRoutingContext () {
+  return useContext(RoutingContext)
+}

41f6ab60a2815a5aeff54c06738a258564f78441
lucasMontenegro <40044087+lucasMontenegro@users.noreply.github.com>
2019-12-20 17:01:46 -0300

    lib/react/utils/Div: tests

--- src/lib/react/utils/__tests__/Div.js
+++ src/lib/react/utils/__tests__/Div.js
@@ -6,48 +6,22 @@ describe(`../Div`, () => {
     expect(jestUtils.getDependencies([`react`, `prop-types`])).toMatchSnapshot()
   })
   {
-    const box = 256
-    const color = `blue`
-    const style = { borderRadius: 8 }
-    const children = `lorem ipsum`
-    const ref = () => {}
     const cases = [
-      [{ box, color, style, children, ref, }],
-      [{      color, style, children, ref, }],
-      [{ box,        style, children, ref, }],
-      [{             style, children, ref, }],
-      [{ box, color,        children, ref, }],
-      [{      color,        children, ref, }],
-      [{ box,               children, ref, }],
-      [{                    children, ref, }],
-      [{ box, color, style,           ref, }],
-      [{      color, style,           ref, }],
-      [{ box,        style,           ref, }],
-      [{             style,           ref, }],
-      [{ box, color,                  ref, }],
-      [{      color,                  ref, }],
-      [{ box,                         ref, }],
-      [{                              ref, }],
-      [{ box, color, style, children,      }],
-      [{      color, style, children,      }],
-      [{ box,        style, children,      }],
-      [{             style, children,      }],
-      [{ box, color,        children,      }],
-      [{      color,        children,      }],
-      [{ box,               children,      }],
-      [{                    children,      }],
-      [{ box, color, style,                }],
-      [{      color, style,                }],
-      [{ box,        style,                }],
-      [{             style,                }],
-      [{ box, color,                       }],
-      [{      color,                       }],
-      [{ box,                              }],
-      [{                                   }],
+      [{ color: `blue` }],
+      [{}],
     ]
     describe.each(cases)(`Div (%j)`, props => {
       it(`should render`, () => {
-        expect(renderer.create(<Div {...props} />).toJSON()).toMatchSnapshot()
+        expect(renderer.create(
+          <Div
+            {...props}
+            box={256}
+            style={{ borderRadius: 8 }}
+            ref={() => {}}
+          >
+            lorem ipsum
+          </Div>
+        ).toJSON()).toMatchSnapshot()
       })
     })
   }

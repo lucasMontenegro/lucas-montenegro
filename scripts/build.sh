335f076d2c311905e2a959d9bc535f1dc6137275
lucasMontenegro <40044087+lucasMontenegro@users.noreply.github.com>
2020-01-16 16:17:07 -0300

    switching to new UI

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 printf '\n> ./scripts/build-server.sh\n' &&
 ./scripts/build-server.sh &&
 printf '\n\n> react-scripts build\n' &&
-./scripts/build-entry-point.sh legacy &&
+./scripts/build-entry-point.sh ui &&
 react-scripts build

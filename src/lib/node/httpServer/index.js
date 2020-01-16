366fbfff2500f95d9d7252a41bf948dcf763ac35
lucasMontenegro <40044087+lucasMontenegro@users.noreply.github.com>
2020-01-16 15:24:59 -0300

    lib/node/httpServer

---
+++ src/lib/node/httpServer/index.js
@@ -0,0 +1,19 @@
+import globals from "lib/utils/globals"
+import express from "express"
+import path from "path"
+import isProduction from "lib/utils/isProduction"
+export default function httpServer (api) {
+  const buildPath = globals.process.env.BUILD_PATH
+  const PORT = globals.process.env.PORT || 5000
+  const app = express()
+  // Priority serve any static files.
+  app.use(express.static(buildPath))
+  // Answer API requests.
+  api && app.use(`/api`, api)
+  // All remaining requests return the React app, so it can handle routing.
+  app.get(`*`, (req, res) => res.sendFile(path.resolve(buildPath, `index.html`)))
+  app.listen(PORT, () => {
+    const name = isProduction() ? `cluster worker ${globals.process.pid}` : `dev server`
+    globals.console.log(`Node ${name}: listening on port ${PORT}`)
+  })
+}

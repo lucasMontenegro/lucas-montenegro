5e1196dff905c56ae0b50abee4494b8b2957069b
lucasMontenegro <40044087+lucasMontenegro@users.noreply.github.com>
2020-01-20 20:18:11 -0300

    lib/NotFound: migration to lib/react/useTranslation

--- src/lib/NotFound/index.test.js
+++ src/lib/NotFound/index.test.js
@@ -4,40 +4,36 @@ import renderer from "react-test-renderer"
 import NotFound from "./index.js"
 jest.mock(`@material-ui/core/styles`, () => ({
   __esModule: true,
-  makeStyles: fn => {
-    fn({
-      spacing: () => {},
-      palette: { text: {} },
+  makeStyles: (styles, options) => () => {
+    const classes = styles({
+      spacing: n => `theme.spacing(${n})`,
+      palette: { text: { secondary: `theme.palette.text.secondary` } },
     })
-    return () => ({
-      root: `classes-root`,
-      content: `classes-content`,
-      referrer: `classes-referrer`,
-      link: `classes-link`,
-    })
+    return Object.keys(classes).reduce((classes, key) => {
+      classes[key] = `${options.name}-${key}(${JSON.stringify(classes[key])})`
+      return classes
+    }, classes)
   },
 }))
-jest.mock(`lib/Translation`, () => ({
-  __esModule: true,
-  default: class Translation {
-    get () {
-      return {
-        title: `translation.title`,
-        error: `translation.error`,
-        greeting: `translation.greeting`,
-        close: `translation.close`,
-        home: `translation.home`,
-      }
-    }
-  },
-}))
-jest.mock(`lib/react/routing/context`, () => ({ __esModule: true, useRoutingContext: jest.fn() }))
+jest.mock(`lib/react/useTranslation`, () => {
+  const React = jest.requireActual("react")
+  return {
+    __esModule: true,
+    default: () => source => (
+      <div className="translation">
+        {Object.keys(source).map(key => (
+          <div key={key} className={key}>{source[key]()}</div>
+        ))}
+      </div>
+    ),
+  }
+})
 jest.mock(`@material-ui/core/Typography`, () => {
   const React = jest.requireActual("react")
   return {
     __esModule: true,
     default: function Typography (props) {
-      return <div {...props} />
+      return <div {...props} className={`Typography ${props.className}`} />
     },
   }
 })
@@ -50,8 +46,9 @@ jest.mock(`lib/react/links/Link`, () => {
     },
   }
 })
+jest.mock(`lib/react/routing/context`, () => ({ __esModule: true, useRoutingContext: jest.fn() }))
 describe(`./index.js`, () => {
-  it(`should use the versions of its dependencies`, () => {
+  it(`should use the right versions of its dependencies`, () => {
     expect(jestUtils.getDependencies([`@material-ui/core`, `react`])).toMatchSnapshot()
   })
   describe(`<NotFound /> (no match)`, () => {
@@ -63,7 +60,7 @@ describe(`./index.js`, () => {
       html = renderer.create(<NotFound />)
     })
     it(`should render`, () => {
-      expect(html.toJSON()).toMatchSnapshot()
+      expect(html.toJSON()).toBeNull()
     })
   })
   describe(`<NotFound /> (match with referrer)`, () => {
@@ -86,24 +83,18 @@ describe(`./index.js`, () => {
   })
   {
     const cases = [
-      [`referrer not in location`, {
-        render: { notFound: true },
-        location: {},
-      }],
-      [`referrer set to undefined`, {
-        render: { notFound: true },
-        location: { state: undefined },
-      }],
-      [`referrer set to null`, {
-        render: { notFound: true },
-        location: { state: null },
-      }],
+      [`referrer not in location`, {}],
+      [`referrer set to undefined`, { state: undefined }],
+      [`referrer set to null`, { state: null }],
     ]
-    describe.each(cases)(`<NotFound /> (match without referrer, %s)`, (msg, route) => {
+    describe.each(cases)(`<NotFound /> (match without referrer, %s)`, (msg, routeLocation) => {
       let html
       beforeAll(() => {
         useRoutingContext.mockReturnValueOnce({
-          route,
+          route: {
+            render: { notFound: true },
+            location: routeLocation,
+          },
           routing: {
             translatedLocations: { home: { get: () => `/translated-home-location` } },
           },

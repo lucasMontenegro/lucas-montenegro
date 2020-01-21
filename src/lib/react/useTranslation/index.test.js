8ed81a4815825e85287ab3445b2e359ccf0dd32f
lucasMontenegro <40044087+lucasMontenegro@users.noreply.github.com>
2020-01-21 17:20:01 -0300

    lib/react/useTranslation: throw error when translation is missing

--- src/lib/react/useTranslation/index.test.js
+++ src/lib/react/useTranslation/index.test.js
@@ -1,9 +1,13 @@
 import languageDetector from "lib/languageDetector"
+import isProduction from "lib/utils/isProduction"
+import globals from "lib/utils/globals"
 import useTranslation from "./index.js"
 jest.mock(`lib/languageDetector`, () => ({
   __esModule: true,
   default: { get: jest.fn() },
 }))
+jest.mock(`lib/utils/isProduction`, () => ({ __esModule: true, default: jest.fn() }))
+jest.mock(`lib/utils/globals`, () => ({ __esModule: true, default: {} }))
 describe(`./index.js`, () => {
   it(`should use the right versions of its dependencies`, () => {
     expect(jestUtils.getDependencies([
@@ -27,12 +31,32 @@ describe(`./index.js`, () => {
         })).toBe(`bar language`)
       })
     })
-    describe(`t (missing translation)`, () => {
-      it(`should return the default translation`, () => {
-        expect(t({
+    describe(`t (missing translation, isProduction => true)`, () => {
+      let result
+      const error = jest.fn()
+      beforeAll(() => {
+        isProduction.mockReturnValueOnce(true)
+        globals.console = { error }
+        result = t({
           foo: () => `foo language`,
           baz: () => `baz language`,
-        })).toBe(`foo language`)
+        })
+      })
+      it(`should return the default translation`, () => {
+        expect(result).toBe(`foo language`)
+      })
+      it(`should log an error message`, () => {
+        expect(error.mock.calls)
+          .toEqual([[`Language code (bar) not supported in translation, using default.`]])
+      })
+    })
+    describe(`t (missing translation, isProduction => false)`, () => {
+      it(`should throw an error`, () => {
+        isProduction.mockReturnValueOnce(false)
+        expect(() => t({
+          foo: () => `foo language`,
+          baz: () => `baz language`,
+        })).toThrow(`Language code (bar) not supported in translation, using default.`)
       })
     })
   })
